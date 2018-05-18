cc33d1674736789627ac0e2303bf5acd6c67c2a1
opringle <ojapringle@gmail.com>
2018-05-17 22:22:44 -0700

    adding final pool layer back

--- vdcnn.py
+++ vdcnn.py
@@ -38,7 +38,7 @@ parser.add_argument('--output-dir', type=str, default='checkpoint',
                     help='directory to save model params/symbol to')
 parser.add_argument('--gpus', type=str, default='',
                     help='list of gpus to run, e.g. 0 or 0,2,5. empty means using cpu.')
-parser.add_argument('--num-epochs', type=int, default=100,
+parser.add_argument('--num-epochs', type=int, default=256,
                     help='how  many times to update the model parameters')
 parser.add_argument('--batch-size', type=int, default=512,
                     help='the number of training records in each minibatch')
@@ -201,11 +201,11 @@ def build_symbol(iterator, preprocessor, blocks, channels):
             pool = mx.sym.Pooling(block, kernel=(1, 3), stride=(1, 2), pad=(0, 1), pool_type='max')
             print('\tblock' + str(i) + '_p', pool.infer_shape(data=X_shape)[1][0])
 
-    # final_pool = mx.sym.Pooling(block, kernel=(1, 16), stride=(1, 16), pad=(0, 1), pool_type='max')
-    # print("final pool output: ", final_pool.infer_shape(data=X_shape)[1][0])
+    final_pool = mx.sym.Pooling(block, kernel=(1, 16), stride=(1, 16), pad=(0, 1), pool_type='max')
+    print("final pool output: ", final_pool.infer_shape(data=X_shape)[1][0])
 
     # Fully connected layers
-    fc1 = mx.sym.FullyConnected(block, num_hidden=2048, flatten=True, name='fc1')
+    fc1 = mx.sym.FullyConnected(final_pool, num_hidden=2048, flatten=True, name='fc1')
     act1 = mx.sym.Activation(fc1, act_type='relu', name='fc1_act')
     print("fc1 output: ", fc1.infer_shape(data=X_shape)[1][0])
 

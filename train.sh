e575330e62448adbd827b2eddc734ec05fdb7113
Training Server <developers@finn.ai>
2018-06-18 15:54:28 -0700

    new hyperparams

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # OK
-# python vdcnn.py --gpus=0 --data=../../finn-dl/MODEL/atb_model_41/strat_split/data/ --sequence-length=128 --dropout=0.0 --final-pool --batch-size=512
+python vdcnn.py --gpus=0 --data=../../finn-dl/MODEL/atb_model_41/strat_split/data/ --sequence-length=64 --batch-size=512 --lr=0.0002 --blocks='10,10,4,4' --channels='64,128,256,512' --hidden-dropout=0.5
 
 # > 76.8% validation score
 # python vdcnn.py --gpus=0 --data=../../finn-dl/MODEL/atb_model_41/strat_split/data/ --sequence-length=128 --final-pool --batch-size=512 --encode --lr=0.0005 --l2=0 --dropout=0.0
@@ -29,4 +29,4 @@
 
 # This should achieve ~ 91.27% validation accuracy
 # python vdcnn.py --gpus=0 --sequence-length=128 --batch-size=128 --lr=0.001 --blocks='5,5,2,2' --channels='64,128,256,512' --final-pool
-python vdcnn.py --gpus=0 --data=../../finn-dl/MODEL/atb_model_41/strat_split/data/ --sequence-length=256 --batch-size=512 --lr=0.0001 --blocks='2,2,2,2' --channels='64,128,256,512'
+# python vdcnn.py --gpus=0 --data=../../finn-dl/MODEL/atb_model_41/strat_split/data/ --sequence-length=256 --batch-size=512 --lr=0.0001 --blocks='2,2,2,2' --channels='64,128,256,512'

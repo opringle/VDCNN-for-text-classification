84282fe5d43c818483d64bd1810bb60fdb52f364
opringle <ojapringle@gmail.com>
2018-07-05 05:28:10 -0700

    done on this branch

--- preprocess_yahoo.py
+++ preprocess_yahoo.py
@@ -13,11 +13,13 @@ def preprocess_df(df, name):
 
     return df[['intent', 'utterance']]
 
-test_df = pd.read_csv('./data/yahoo_answers_csv/test.csv', names=['intent', 'utterance', 'question_content', 'best_answer'])
-train_df = pd.read_csv('./data/yahoo_answers_csv/train.csv', names=['intent', 'utterance', 'question_content', 'best_answer'])
+test_df = pd.read_csv('./data/yahoo_answers/test.csv', names=['intent', 'utterance', 'question_content', 'best_answer'])
+train_df = pd.read_csv('./data/yahoo_answers/train.csv', names=['intent', 'utterance', 'question_content', 'best_answer'])
 
+print(train_df[['utterance']][train_df.utterance.str.startswith("What should I look for when buying a laptop?")].head())
+print(train_df.iloc[153, 1])
 test_df = preprocess_df(test_df, 'test')
 train_df = preprocess_df(train_df, 'train')
 
-train_df.to_pickle('./data/yahoo_train.pickle')
-test_df.to_pickle('./data/yahoo_test.pickle')
+# train_df.to_pickle('./data/yahoo_train.pickle')
+# test_df.to_pickle('./data/yahoo_test.pickle')
--- train.sh
+++ train.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # ATB data (85.8 to beat)
-#python vdcnn.py --gpus=0 --data=../../finn-dl/MODEL/atb_model_41/strat_split/data/ --output-dir=atb_model \
-#--checkpoint-freq=1 \
-#--num-epochs=256 --batch-size=512 --sequence-length=107 --char-embed=16 \
-#--lr=0.00006 \
-#--l2=0.0 --dropout=0.8 --smooth-alpha=0.001 \
-#--blocks=4,7,3
+python vdcnn.py --gpus=0 --data=../../finn-dl/MODEL/atb_model_41/strat_split/data/ --output-dir=atb_model \
+--checkpoint-freq=1 --load-prefix=yahoo_model/checkpoint --load-epoch=11 \
+--num-epochs=256 --batch-size=512 --sequence-length=107 --char-embed=16 \
+--lr=0.00006 \
+--l2=0.0 --dropout=0.8 --smooth-alpha=0.001 \
+--blocks=4,7,3
 
 # AGNEWs data (91.33 to beat)
 #python vdcnn.py --gpus=0 --data=./data/ \
@@ -16,9 +16,9 @@
 #--blocks=4,7,3
 
 # YAHOO data (73.43 to beat)
-python vdcnn.py --gpus=0 --data=./data/yahoo_answers/ --output-dir=yahoo_model --checkpoint-freq=1 \
---checkpoint-freq=1 \
---num-epochs=256 --batch-size=512 --sequence-length=107 --char-embed=16 \
---lr=0.00006 \
---l2=0.0 --dropout=0.8 --smooth-alpha=0.001 \
---blocks=4,7,3
+#python -u vdcnn.py --gpus=0 --data=./data/yahoo_answers/ --output-dir=yahoo_model --checkpoint-freq=1 \
+#--checkpoint-freq=1 \
+#--num-epochs=256 --batch-size=512 --sequence-length=107 --char-embed=16 \
+#--lr=0.00006 \
+#--l2=0.0 --dropout=0.8 --smooth-alpha=0.001 \
+#--blocks=4,7,3
